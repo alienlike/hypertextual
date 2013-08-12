8f2abcb86b260cf0535ed4f881436a7e7477a220
Nathaniel Wingfield <nwingfield@fastmail.net>
2013-08-11 23:19:37 -0400

    cleaned up styles; added some styles for devices

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-SITE_URL = os.environ.get('HYPERTEXTUAL_SITE_URL', 'http://localhost:5000')
+SITE_URL = os.environ.get('HYPERTEXTUAL_SITE_URL', 'http://10.0.1.10:5000')
 PORT = int(os.environ.get('HYPERTEXTUAL_PORT', '5000')) # this setting ignored by gunicorn
 CONN_STR = os.environ.get('DATABASE_URL', 'postgresql://nw@localhost:5432/hypertextual')
 CONN_STR_TEST = 'postgresql://nw@localhost:5432/hypertextual_test'
